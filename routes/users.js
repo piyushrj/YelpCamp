818d91a1063a70e522a86550359f5805c9cf6b19
Piyush Raj <piyushrajiitr@gmail.com>
2022-07-31 21:08:43 +0530

    Added basic authentication - register, login, logout

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+
+router.get('/register', (req, res) => {
+    res.render('users/register');
+})
+
+router.post('/register', catchAsync(async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ 'email': email, 'username': username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        })
+    }
+    catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    console.log(res.locals.returnTo);
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+})
+
+module.exports = router;
