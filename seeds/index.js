1c581ebd586ee667669a46fd8d9b11b0e3ad64e9
Piyush Raj <piyushrajiitr@gmail.com>
2022-08-01 16:49:18 +0530

    Added authorization for campgrounds and reviews

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '62e7a564e6cc742cff896fee',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
