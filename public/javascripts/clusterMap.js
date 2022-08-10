4173fa4df13b1048749b676b26fced474d883bc8
Piyush Raj <piyushrajiitr@gmail.com>
2022-08-10 15:20:58 +0530

    styles clean up and homepage redesign

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,11 +1,14 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
     center: [-103.5917, 40.6699],
     zoom: 3
 });
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
@@ -102,9 +105,9 @@ map.on('load', () => {
     // the location of the feature, with
     // description HTML from its properties.
     map.on('click', 'unclustered-point', (e) => {
-        const {popUpMarkup} = e.features[0].properties;
+        const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
-        
+
         // Ensure that if the map is zoomed out such that
         // multiple copies of the feature are visible, the
         // popup appears over the copy being pointed to.
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,6 +7,10 @@ const map = new mapboxgl.Map({
     zoom: 10, // starting zoom
     projection: 'globe' // display the map as a 3D globe
 });
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
